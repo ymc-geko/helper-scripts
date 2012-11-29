4851e57c93f7d1e556d586a8f0901b5db45331a3
Gerd Koenig <ymc-geko@vcs.ymc.ch>
2012-11-29 16:29:30 +0100

    script to delete an ssh key in local known_hosts file

---
+++ del_host_key.sh
@@ -0,0 +1,25 @@
+#/bin/bash -x
+#
+# Description:
+#   deletes the given line in the known_hosts file
+#   located at ~/.ssh/known_hosts. If a ssh connection will be
+#   refused due to an offending key, the error message contains
+#   the line number. Just put this line number as parameter to
+#   this script.
+#
+# written by Gerd Koenig <gerd.koenig@ymc.ch>
+#
+knownHostsFile=~/.ssh/known_hosts
+
+if [ -z $1 ]; then
+  echo "no line no. given. Provide the line number as parameter."
+  exit 1
+fi
+if [ ! -f $knownHostsFile ]; then
+  echo "known_hosts file not found"
+  exit 1
+fi
+echo "deleting line no. $1 from known_hosts file"
+sed -i "$1d" $knownHostsFile
+echo "..done"
+exit 0
